15698a931745568a9b8b2f17e302fd699e0d1fc0
AtmosDerrick <93062640+AtmosDerrick@users.noreply.github.com>
2023-11-10 16:49:29 -0800

    done with authentication

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save 
 
 # Create your models here.
 class Profile(models.Model):
@@ -17,6 +17,7 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.User.username
+
 
 
     
